55fd6f247e8fbfcea5b8414f5fadfb568c22272a
Aliciaa33 <160833715+Aliciaa33@users.noreply.github.com>
2025-04-15 10:15:55 +0800

    Update Login & Register
    update the login & register codes. Also, add view handles to handle the submission.

--- curesell/user/urls.py
+++ curesell/user/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.welcome),
-    url(r'^$', views.homepage),
-    url(r'^login/$',views.login),
-    url(r'^register/$',views.register),
-    url(r'^verification/$',views.verification),
+    url(r'^login/$', views.login,name="login"),
+    url(r'^register/$',views.register,name="register"),
+    url(r'^register_handle/$',views.register_handle),
+    url(r'^login_handle/$',views.login_handle),
+    url(r'^logout/$',views.logout),
+    url(r'^user/homepage/$',views.homepage,name='homepage'),
+    url(r'^verification/', views.verification, name='verification'),
+    url(r'^verification_handle/$',views.verification_handle),
 ]
